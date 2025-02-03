52b7ba73ca142a20eb88215025a298bee3420d10
joaquinn01 <143659585+joaquinn01@users.noreply.github.com>
2025-02-03 05:50:12 +0000

    Finished lab

--- src/BankAccount.java
+++ src/BankAccount.java
@@ -1,1 +1,14 @@
+public class BankAccount{
+    public static void main(String[] args){
+        double balance = 2175.37;
+        balance -= 302.50;
+        balance += 50.03;
+        balance /= 2.0;
+        balance += 20.00;
+        balance -= 1.00;
+        balance *= 2.0;
+        balance += 1.0;
 
+        System.out.printf("The final balance is: %.2f", balance);
+    }
+}
